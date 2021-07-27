57293acc6e1670d3fa425de4d566977b8a5626fe
Mikhail Podalyako <mihailpodalyako1@gmail.com>
2021-07-27 23:52:19 +0500

    Добавил кнопку, повесил на нее ClickListener

--- app/src/main/java/com/gmail/homework31/MainActivity.kt
+++ app/src/main/java/com/gmail/homework31/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.gmail.homework31
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Button is clicked!", Toast.LENGTH_SHORT).show()
+
+            }
+
+        })
     }
 }
