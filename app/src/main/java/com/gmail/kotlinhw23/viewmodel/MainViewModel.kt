79e84afbfea19a48a31e8db0ed9ae7e372b2124e
Mikhail Podalyako <mihailpodalyako1@gmail.com>
2021-08-02 22:31:01 +0500

    View model + LiveData

--- app/src/main/java/com/gmail/kotlinhw23/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/gmail/kotlinhw23/viewmodel/MainViewModel.kt
@@ -4,13 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
 class MainViewModel (private val liveDataToObserver: MutableLiveData<Any> = MutableLiveData())
     : ViewModel() {
 
         fun getData():LiveData<Any>{
+            getDataFromLocaleSourse()
             return liveDataToObserver
         }
+
+    private fun getDataFromLocaleSourse(){
+        Thread{
+            sleep(2000)
+            liveDataToObserver.postValue(Any())
+        }.start()
+    }
 
     // TODO: Implement the ViewModel
 }
