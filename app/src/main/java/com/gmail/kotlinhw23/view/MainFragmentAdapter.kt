1df8c93200b6dd7c5db4f00abd686cf2371a72e9
Mikhail Podalyako <mihailpodalyako1@gmail.com>
2021-08-08 23:15:22 +0500

    HW4

--- app/src/main/java/com/gmail/kotlinhw23/view/MainFragmentAdapter.kt
+++ app/src/main/java/com/gmail/kotlinhw23/view/MainFragmentAdapter.kt
@@ -11,14 +11,10 @@ class MainFragmentAdapter:
 RecyclerView.Adapter<MainFragmentAdapter.MainViewHolder>(){
 
     private var weatherData: List<Weather> = listOf()
-    private var onItemViewClickListener: MainFragment.OnItemViewClickListener? = null
+    private var onItemViewClickListener: (Weather) -> Unit = {}
 
-    fun setOnItemViewClickListener(onItemViewClickListener: MainFragment.OnItemViewClickListener){
+    fun setOnItemViewClickListener(onItemViewClickListener: (Weather) -> Unit){
         this.onItemViewClickListener = onItemViewClickListener
-    }
-
-    fun removeOnItemViewClickListener(){
-        onItemViewClickListener = null
     }
 
     fun setWeather(data: List<Weather>) {
@@ -43,9 +39,11 @@ RecyclerView.Adapter<MainFragmentAdapter.MainViewHolder>(){
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(weather: Weather) {
-            binding.mainFragmentRecyclerItemTextView.text = weather.city.city
-            binding.root.setOnClickListener {
-                onItemViewClickListener?.onItemViewClick(weather)
+            binding.apply {
+                mainFragmentRecyclerItemTextView.text = weather.city.city
+                root.setOnClickListener {
+                    onItemViewClickListener(weather)
+                }
             }
         }
     }
